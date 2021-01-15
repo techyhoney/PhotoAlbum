cde53cee24ce266c68a21ac8afa1edb925190fb8
Hiten <hitengoyal18@gmail.com>
2021-01-15 16:22:13 +0530

    Updated Files

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './Components/Main'
+import './styles/stylesheet.css'
+import {BrowserRouter} from 'react-router-dom'
 
 
-class List extends React.Component {
-    render() {
-
-        const tasks = this.props.tasks;
-        
-        
-        
-        return <div>
-                      <ol> 
-                         { tasks.map((task, index) => <li key = {index}> {task} </li>)}
-                     </ol>
-                 </div>
-        
-
-    }
-
-}
-
-class Title extends React.Component {
-    render() {
-        return <h1> Task List </h1>
-    }
-
-}
-
-
-class Main extends React.Component {
-
-    render() {
-        return <div> 
-            <Title/>
-            <List tasks = {['mow the lawn', 'walk the dog']}/>
-            <List tasks = {['hose the driveway', 'finish the laundry']}/>
-         </div>
-
-}
-
-}
-
-ReactDOM.render(<Main/>, document.getElementById('root'));
-
-
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'))
